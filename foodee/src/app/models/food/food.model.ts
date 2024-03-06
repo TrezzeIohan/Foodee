41d961babf93c4da03a289425279f56e4ee41e4b
Iohan <iohan.trezze@gmail.com>
2024-03-05 21:55:36 -0300

    Food model done

--- foodee/src/app/models/food/food.model.ts
+++ foodee/src/app/models/food/food.model.ts
@@ -2,6 +2,9 @@ import { Ingredient } from "./ingredient/ingredient.model";
 import { Nutrition } from "./ingredient/nutrition/nutrition.model";
 
 export class Food {
+    public nutritionalValue: Nutrition;
+    public diaryNutriValues: Map<string, number>;
+
     constructor(
       public id: number,
       public name: string,
@@ -9,20 +12,25 @@ export class Food {
       public ingredients: Ingredient[],
       public price: number,
       public offer: boolean,
-      public nutritionalValue: Nutrition, // Talvez eu precise criar um objeto para valores nutricionais
       public suggestion: boolean,
+      public image: string,
+
     // I didn't used a function to determine tags, because I thought the client would
     // want to detemine it because of marketing reasons, etc
       public tags: string[],
-    ) {}
+    ) {
+      this.nutritionalValue = this.calculateNutritionalValue();
+      this.diaryNutriValues = this.calculateDiaryNutriValues();
+    }
 
     //Changes the suggestion property
     turnSuggestionOnOff(): void{
       this.suggestion = !this.suggestion
     }
 
-    getNutritionalValue(): Nutrition{
-    let totalNutritionalValue: Nutrition = { calories: 0, carbs: 0, fat: 0, protein: 0 };;
+    // Sums the nutritional values of the ingredients and returns a nutrition object with the sums
+    calculateNutritionalValue(): Nutrition{
+    let totalNutritionalValue: Nutrition = { calories: 0, carbs: 0, fat: 0, protein: 0 };
       this.ingredients.forEach(ingredient => {
         totalNutritionalValue.calories += ingredient.nutritionalValue.calories;
         totalNutritionalValue.carbs += ingredient.nutritionalValue.carbs;
@@ -32,5 +40,18 @@ export class Food {
     return totalNutritionalValue;
     }
 
+    calculateDiaryNutriValues(): Map<string, number>{
+      let dailyCarbs = 250;
+      let dailyFat = 44;
+      
+      let dailyCarbsPercentage = (100 * this.nutritionalValue.carbs)/dailyCarbs;
+      let dailyFatPercentage = (100 * this.nutritionalValue.fat)/dailyFat;
+
+      let diaryNutriValues = new Map <string, number>();
+      diaryNutriValues.set('dailyCarbsPercentage', dailyCarbsPercentage);
+      diaryNutriValues.set('dailyFatPercentage', dailyFatPercentage);
+
+      return diaryNutriValues;
+    }
 
   }
--- foodee/src/app/models/food/ingredient/ingredient.model.ts
+++ foodee/src/app/models/food/ingredient/ingredient.model.ts
@@ -4,11 +4,13 @@ import { Nutrition } from "./nutrition/nutrition.model";
     name: string;
     price: number;
     nutritionalValue: Nutrition;
+    image: string;
 
-    constructor(id: string, name: string, price: number, nutritionalValue: Nutrition) {
+    constructor(id: string, name: string, price: number, nutritionalValue: Nutrition, image: string) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.nutritionalValue = nutritionalValue;
+        this.image = image;
     }
 }
