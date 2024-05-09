7b8622945c6d2d216e9569d554b797bdd6f3b2fb
Iohan Trézze <iohan.trezze@gmail.com>
2024-05-09 16:57:32 -0300

    Setting up the slider

--- foodee/src/app/main/main.component.ts
+++ foodee/src/app/main/main.component.ts
@@ -10,35 +10,19 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
 })
 export class MainComponent {
 slides = [
-    {img: "http://placehold.it/350x150/000000"},
-    {img: "http://placehold.it/350x150/111111"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/666666"}
+    {id: 1, img: "../assets/food-imgs/donut.svg", title: "Free Donut!", description:"For orders over $20"},
+    {id: 2, img: "../assets/food-imgs/donut.svg", title: "Shakes", description:"Delicious"},
+    {id: 3, img: "../assets/food-imgs/donut.svg", title: "", description:"Delicious"},
+    {id: 4, img: "../assets/food-imgs/donut.svg", title: "", description:"Delicious"}
   ];
-  slideConfig = {"slidesToShow": 4, "slidesToScroll": 4};
-  
-  addSlide() {
-    this.slides.push({img: "http://placehold.it/350x150/777777"})
-  }
-  
-  removeSlide() {
-    this.slides.length = this.slides.length - 1;
-  }
+  slideConfig = {
+    "slidesToShow": 1, 
+    "slidesToScroll": 1, 
+    "arrows": false,
+  };
   
   slickInit(e: any) {
     console.log('slick initialized');
-  }
-  
-  breakpoint(e: any) {
-    console.log('breakpoint');
-  }
-  
-  afterChange(e: any) {
-    console.log('afterChange');
-  }
-  
-  beforeChange(e: any) {
-    console.log('beforeChange');
   }
 
 }
