f1f014500fee6677d2a6471080a591b3ba10a372
Iohan <iohan.trezze@gmail.com>
2024-05-20 22:04:50 -0300

    Some changes to models and frontend

--- foodee/src/app/fakedb.ts
+++ foodee/src/app/fakedb.ts
@@ -4,11 +4,11 @@ import { Nutrition } from './models/food/ingredient/nutrition/nutrition.model';
 
 // Ingredientes
 const cheese = new Ingredient('1', 'Cheese', 2.0, new Nutrition(100, 1, 8, 6), 'cheese.jpg');
-const tomato = new Ingredient('2', 'Tomato', 0.5, new Nutrition(20, 4, 0.2, 1), 'tomato.jpg');
+const tomato = new Ingredient('2', 'Tomato', 0.5, new Nutrition(20, 4, 0.2, 1), './assets/imgs/tomato.svg');
 const dough = new Ingredient('3', 'Dough', 1.5, new Nutrition(200, 40, 2, 6), 'dough.jpg');
-const cheddar = new Ingredient('1', 'Cheddar', 1.5, new Nutrition(113, 0.1, 9.4, 7.0), 'cheddar.jpg');
-const onions = new Ingredient('2', 'Onions', 0.5, new Nutrition(40, 9.0, 0.1, 1.1), 'onions.jpg');
-const bacon = new Ingredient('3', 'Bacon', 2.0, new Nutrition(42, 0.2, 3.3, 3.0), 'bacon.jpg');
+const cheddar = new Ingredient('1', 'Cheddar', 1.5, new Nutrition(113, 0.1, 9.4, 7.0), './assets/imgs/cheddar.svg');
+const onions = new Ingredient('2', 'Onions', 0.29, new Nutrition(40, 9.0, 0.1, 1.1), './assets/imgs/onion.svg');
+const bacon = new Ingredient('3', 'Bacon', 0.59, new Nutrition(42, 0.2, 3.3, 3.0), './assets/imgs/bacon.svg');
 const pepperoni = new Ingredient('4', 'Pepperoni', 2.5, new Nutrition(150, 1, 13, 6), 'pepperoni.jpg');
 const mushroom = new Ingredient('5', 'Mushroom', 1.0, new Nutrition(15, 2, 0.1, 1), 'mushroom.jpg');
 
@@ -35,7 +35,7 @@ const cheeseburger = new Food(
   true,
   false,
   '../assets/imgs/hamburger.svg',
-  [],
+  [cheddar, bacon, onions, tomato],
   ['Classic Taste', 'Bestseller']
 );
 
--- foodee/src/app/food-page/food-page.component.ts
+++ foodee/src/app/food-page/food-page.component.ts
@@ -29,12 +29,6 @@ export class FoodPageComponent {
    
   }
   
-  slides = [
-    {id: 1, img: "../assets/imgs/donut.svg", title: "Cheddar", description:"+0.23", bgColorClass:"bg-color-cambridge"},
-    {id: 2, img: "../assets/imgs/fries.svg", title: "Extra Large Fries", description:"Free for orders over $35", bgColorClass:"bg-color-hunyadi-yellow"},
-    {id: 3, img: "../assets/imgs/ingredients/bacon.svg", title: "Two For One", description:"Order an extra bacon and get two", bgColorClass:"bg-color-light-coral"},
-    {id: 4, img: "../assets/imgs/choc.svg", title: "Choco Friday", description:"Chocolate festival for choco lovers", bgColorClass:"bg-color-tropical-indigo"}
-  ];
   slideConfig = {
     "slidesToShow": 3, 
     "slidesToScroll": 3, 
