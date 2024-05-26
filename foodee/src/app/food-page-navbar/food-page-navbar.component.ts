fb45d67bc3e09bdd0cf7c3e78924ec6b9b45a512
Iohan <iohan.trezze@gmail.com>
2024-05-26 13:05:34 -0300

    Fpnavbar css done

--- foodee/src/app/food-page-navbar/food-page-navbar.component.ts
+++ foodee/src/app/food-page-navbar/food-page-navbar.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-food-page-navbar',
   standalone: true,
-  imports: [],
+  imports: [ NgIf],
   templateUrl: './food-page-navbar.component.html',
   styleUrl: './food-page-navbar.component.sass'
 })
 export class FoodPageNavbarComponent {
+  numberOfItems: number = 0;
 
+  increaseNumberOfItems(){
+    this.numberOfItems++;
+  }
+
+  decreaseNumberOfItems(){
+    if(this.numberOfItems > 0){
+      this.numberOfItems--;
+    }
+  }
 }
