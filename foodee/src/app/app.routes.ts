e25d7ee3520ee7a9814ffe35b78a732a5c87a384
Iohan Trezze <1202@appai.org.br>
2024-06-04 15:20:29 -0300

    Cart page started

--- foodee/src/app/app.routes.ts
+++ foodee/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { FoodPageComponent } from './food-page/food-page.component';
+import { MyCartComponent } from './my-cart/my-cart.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
     { path: 'main', component: MainComponent },
     { path: 'food-page/:id', component: FoodPageComponent},
-    { path: '**', redirectTo: '/home', pathMatch: 'full' }  // Rota para páginas não encontradas (opcional)
+    { path: 'my-cart', component: MyCartComponent},
+    { path: '**', redirectTo: '/main', pathMatch: 'full' }  // Rota para páginas não encontradas (opcional)
 ];
