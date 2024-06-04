dd60fe72d5e3fbfa04a59ed942f4a410cb964e85
Iohan Trezze <1202@appai.org.br>
2024-06-04 15:43:43 -0300

    Trying to make the stepper work

--- foodee/src/app/my-cart/my-cart.component.ts
+++ foodee/src/app/my-cart/my-cart.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './my-cart.component.sass'
 })
 export class MyCartComponent {
+const stepButtons = document.querySelectorAll('.step-button');
+const progress = document.querySelector('#progress');
+
+Array.from(stepButtons).forEach((button,index) => {
+    button.addEventListener('click', () => {
+        this.progress.setAttribute('value', index * 100 /(this.stepButtons.length - 1) );//there are 3 buttons. 2 spaces.
+
+        this.stepButtons.forEach((item, secindex)=>{
+            if(index > secindex){
+                item.classList.add('done');
+            }
+            if(index < secindex){
+                item.classList.remove('done');
+            }
+        })
+    })
+})
 
 }
