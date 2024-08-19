77760acfe111b735632e524ae66302f7e69e3bd6
TrezzeIohan <iohan.trezze@gmail.com>
2024-08-19 09:59:00 -0300

    Food-list component

--- foodee/src/app/food-list/food-list.component.ts
+++ foodee/src/app/food-list/food-list.component.ts
@@ -3,11 +3,14 @@ import { NavigationService } from '../navigation.service';
 import { DataService } from '../data.service';
 import { ActivatedRoute } from '@angular/router';
 import { FoodCategory } from '../models/food-category/food-category.model';
+import { Food } from '../models/food/food.model';
+import { RouterLink } from '@angular/router';
+import { NgFor, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-food-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgFor, NgForOf],
   templateUrl: './food-list.component.html',
   styleUrl: './food-list.component.sass'
 })
@@ -22,8 +25,11 @@ export class FoodListComponent {
     this.navigationService.goBackToParent();
   }
 
+  foodList: Food[] = this.dataService.getFoodList();
+
   categoryId = parseInt(this.route.snapshot.paramMap.get('id') || '0', 10);
   categories : FoodCategory[] = this.dataService.getCategoryList();
 
-  selectedCategory = this.categories.find(category => category.id === this.categoryId);
+  selectedCategory = this.categories.find(category => category.id === this.categoryId) || { id: null, name: 'Unknown', foodIds: [] };
+
 }
